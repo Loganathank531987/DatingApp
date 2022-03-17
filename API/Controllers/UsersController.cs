5522d483a5b007cceca91dbdd8e25e3b78167e48
LoganathanK531987 <loga.ece@gmail.com>
2022-03-17 01:14:32 -0500

    End Of Section 10

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,15 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using API.Data;
 using API.DTOs;
-using API.Entities;
 using API.Interfaces;
 using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
 
 namespace API.Controllers
 {
@@ -38,6 +32,18 @@ namespace API.Controllers
         {
             return  await _userRepository.GetMemberAsync(username);
         
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDTo)
+        {
+          var username=User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+          var user= await _userRepository.GetUserByUsernameAsync(username);
+          _mapper.Map(memberUpdateDTo,user);
+          _userRepository.Update(user);
+
+          if(await _userRepository.SaveAllAsync()) return NoContent();
+          return BadRequest("Failed to Update User");
         }
     }
 }
