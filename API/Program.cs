579bbbdb0efcd44d559bd642078df39272d994b3
LoganathanK531987 <loga.ece@gmail.com>
2022-05-12 16:42:00 -0500

    Upgraded to Dotnet 6

--- API/Program.cs
+++ API/Program.cs
@@ -1,18 +1,61 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using API.Data;
-using API.Entities;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
-namespace API
+
+var builder = WebApplication.CreateBuilder(args);
+
+// add services to the container
+
+            builder.Services.AddApplicationServices(builder.Configuration);
+            builder.Services.AddControllers();
+            builder.Services.AddCors();
+            builder.Services.AddIdentityServices(builder.Configuration);
+            builder.Services.AddSignalR();
+
+//Configure the HTTP request pipeline
+            var app= builder.Build();
+            app.UseMiddleware<ExceptionMiddleware>();
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+            app.UseCors(b=>b.AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials()
+            .WithOrigins("https://localhost:4200"));
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+           
+          
+            app.MapControllers();
+            app.MapHub<PresenceHub>("hubs/presence");
+            app.MapHub<MessageHub>("hubs/message");
+            app.MapFallbackToController("Index","FallBack");
+
+         
+           using var scope= app.Services.CreateScope();
+           var services= scope.ServiceProvider;
+           try
+           {
+                var context= services.GetRequiredService<DataContext>();   
+                var userManager= services.GetRequiredService<UserManager<AppUser>>();
+                var roleManager= services.GetRequiredService<RoleManager<AppRole>>();
+                await context.Database.MigrateAsync();
+                await Seed.SeedUsers(userManager, roleManager);
+           }
+           catch(Exception ex)
+           {
+              var logger= services.GetRequiredService<ILogger<Program>>();
+              logger.LogError(ex,"An error occured during migration");
+           }
+
+           await app.RunAsync();
+
+          
+           
+
+/*namespace API
 {
     public class Program
     {
@@ -45,4 +88,4 @@ namespace API
                     webBuilder.UseStartup<Startup>();
                 });
     }
-}
+}*/
